b50f48d8c205419a6f3a4a07ba8acd6f6cf9666e
JOHN ESPINAL <john_espinal@bhdleon.com.do>
2022-05-25 16:02:36 -0400

    Better structure

--- useFetch/useFetch.js
+++ useFetch/useFetch.js
@@ -25,18 +25,12 @@ export const useFetch = ( url ) => {
             } )
             .then( data => {
               if(isMounted.current){
-
-                console.log("Se llamo Set state");
-                
                 setState({
                   loading: false,
                   error: null,
                   data
                 }
                 )
-                
-              }else{
-              console.log("No se llamo Set state");
                 
               }
             } )
@@ -48,7 +42,6 @@ export const useFetch = ( url ) => {
               })
               
             } )
-// eslint-disable-next-line
     }, [url])
 
   return state;
